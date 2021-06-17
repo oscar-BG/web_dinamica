26223bad2bae8e242b8807a9b3a75ad14d35906d
Oscar Bautista <oscar04262000@gmail.com>
2021-06-16 23:34:17 -0500

    conexion con heroku

--- modelo/conexionBD.php
+++ modelo/conexionBD.php
@@ -2,7 +2,7 @@
     class Conexion{
         static public function cBD(){
             try{
-                $bd = new PDO("mysql:host=localhost;dbname=panel_control","root","");
+                $bd = new PDO("mysql:host=us-cdbr-east-04.cleardb.com;dbname=heroku_c375f","b3d5f8c447532e","e1ccb828");
                 return $bd;
             }catch(PDOException $e){
                 echo "Error ".$e->getMessage();
